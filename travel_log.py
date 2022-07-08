fbcfad2a8eebf7e5f1d47b09615da23edb71b67b
Melissa Calder-Mason <jyenese@hotmail.com>
2022-07-08 13:01:24 +1000

    added options, first definitions

---
+++ travel_log.py
@@ -0,0 +1,18 @@
+def print_options():
+    print("1. Travel Directory")
+    print("2. Check travel log leaderboard")
+    print("3. Add Future/Previous travels.")
+    print("4. Delete a destination from the menu")
+    print("5. Exit")
+    option = input("Select your option (1-5): ")
+    return option
+
+def travel_log():
+    pass            
+def leaderboard():
+    pass
+def add_travel():
+    pass
+def remove_travel():
+    pass
+
