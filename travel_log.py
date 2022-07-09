7e2dfc95a97c0ef46fac09a7acd0c7edc42d789d
Melissa Calder-Mason <jyenese@hotmail.com>
2022-07-09 12:37:35 +1000

    added seed, also added the add_travels into main

--- travel_log.py
+++ travel_log.py
@@ -1,28 +1,7 @@
 from os import system
-from travel_directory import TravelDirectory
 from travel_data import Travel_data
+from seed import seed
 
-dest1 = Travel_data("Queenstown, New Zealend", "20/7/2015", "10 Days", 8.5)
-dest2 = Travel_data("Bali", "31/8/2015","7 Days", 6.5)
-dest3 = Travel_data("Budapest","28/7/2016","4 Days", 7)
-dest4 = Travel_data("Lagos, Lisbon", "2/8/2016","5 Days", 8.5)
-dest5 = Travel_data("Barcelona, Spain", "7/8/2016","5 Days", 8)
-dest6 = Travel_data("Prague, Czech Republic", "12/8/2016","3 Days", 6)
-dest7 = Travel_data("Hvar, Croatia", "15/8/2016","6 Days", 9)
-dest8 = Travel_data("Split, Croatia", "21/8/2016","2 Days" ,5)
-dest9 = Travel_data("Big White(Ski, Resort), Canada", "26/12/2016","5 Days", 9)
-dest9 = Travel_data("Whistler(Ski Resort), Canada", "1/1/2017", "5 Days", 6)
-dest9 = Travel_data("Las Vegas, Nevada", "23/6/2017","3 Days", 8)
-dest10 = Travel_data("Santa Barbara, California","26/6/2017","3 Days", 5)
-dest11 = Travel_data("San Francisco", "29/6/2017","4 Days", 4)
-dest12 = Travel_data("San Diego", "3/7/2017", "5 Days", 8)
-dest13 = Travel_data("Niseko, Japan", "7/1/2018", "10 Days", 7.5)
-dest14 = Travel_data("Bali", "16/1/2019","7 Days", 8)
-dest15 = Travel_data("Niseko, Japan","7/3/2020", "7 Days", 9)
-
-
-
-travel_dict = TravelDirectory([dest1, dest2, dest3, dest4, dest5, dest6, dest7, dest8, dest9, dest10, dest11, dest12, dest13, dest14, dest15])
 
 def print_options():
     print("1. Travel Directory")
@@ -34,15 +13,19 @@ def print_options():
     option = input("Select your option (1-6): ")
     return option
 
-def travel_log():
-    pass            
+travel_dict = seed()
+
 def leaderboard():
     pass
 def add_travel():
-    pass
+    name = input("What was the name of location? ")
+    date = input(f"What was the date you went to {name}? e.g 22/2/22:   ")
+    duration = input(f"How long were you at {name}? e.g 5 Days:     ")
+    rating = input(f"What would you rate the trip when you went to {name} on {date} for {duration}? (1 being worst, 10 being best):      ")
+    travel_dict.add_travels(name, date, duration, rating)
 def edit_travel():
     pass
-def remove_travel():
+def remove_travels():
     pass
 
 option = ""
@@ -58,7 +41,7 @@ while option != "6":
     elif option == "3":
         add_travel()
     elif option == "4":
-        remove_travel()
+        remove_travels()
     elif option == "5":
         edit_travel()
         continue
